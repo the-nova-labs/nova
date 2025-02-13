939784e5cdb131e93ff5a5193bbef4048c2dfd72
Amanda Casadei <amandacasadei@gmail.com>
2025-02-13 18:56:22 +0000

    fix PSICHIC paths

--- PSICHIC/runtime_config.py
+++ PSICHIC/runtime_config.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+import os
 
 class RuntimeConfig:
+    PSICHIC_PATH = os.path.dirname(os.path.abspath(__file__))
     DEVICE = 'cpu'
-    MODEL_PATH = 'trained_weights/PDBv2020_PSICHIC'
+    MODEL_PATH = os.path.join(PSICHIC_PATH, 'trained_weights', 'PDBv2020_PSICHIC')
     BATCH_SIZE = 128
     
