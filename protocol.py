72a094d3287be3631efd945db3a9a16eae7b919c
urdof7 <urdof7@gmail.com>
2025-02-14 19:31:54 -0500

    Simple protocol for miner-validator communication

--- protocol.py
+++ protocol.py
@@ -1,19 +1,18 @@
 import typing
 import bittensor as bt
 
-class Dummy(bt.Synapse):
-    """
-    A simple dummy protocol that inherits from bt.Synapse.
-    This protocol handles dummy request and response communication between
-    the miner and the validator.
 
+class ChallengeSynapse(bt.Synapse):
+    """
     Attributes:
-    - dummy_input: An integer value representing the input request sent by the validator.
-    - dummy_output: An optional integer value representing the response from the miner.
+    - target_protein (str): The protein or query sent by the validator.
+    - product_name (typing.Optional[str]): The miner's proposed molecule ID.
+      This is optional because the validator only sets the target_protein,
+      while the miner sets product_name in the response.
     """
+    
+    # Required request input, provided by the validator.
+    target_protein: str
 
-    # Required request input, filled by the dendrite caller.
-    dummy_input: int
-
-    # Optional request output, filled by the axon responder.
-    dummy_output: typing.Optional[int] = None
+    # Optional response output, provided by the miner.
+    product_name: typing.Optional[str] = None
