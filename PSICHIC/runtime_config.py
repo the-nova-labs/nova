937e3954ee2e06244f68bf12443bdd575ba413d4
cosmic-amanita <68027389+cosmic-amanita@users.noreply.github.com>
2025-03-01 13:50:12 -0300

    Use GPU

--- PSICHIC/runtime_config.py
+++ PSICHIC/runtime_config.py
@@ -3,7 +3,7 @@ import os
 
 class RuntimeConfig:
     PSICHIC_PATH = os.path.dirname(os.path.abspath(__file__))
-    DEVICE = 'cpu'
+    DEVICE = 'cuda:0'
     MODEL_PATH = os.path.join(PSICHIC_PATH, 'trained_weights', 'PDBv2020_PSICHIC')
     BATCH_SIZE = 128
     
