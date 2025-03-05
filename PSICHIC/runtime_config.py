e16bfa1e3750c3002c356abc9cb5f7dc42b17086
cosmic-amanita <68027389+cosmic-amanita@users.noreply.github.com>
2025-03-05 10:27:33 -0500

    fix device override

--- PSICHIC/runtime_config.py
+++ PSICHIC/runtime_config.py
@@ -3,7 +3,8 @@ import os
 
 class RuntimeConfig:
     PSICHIC_PATH = os.path.dirname(os.path.abspath(__file__))
-    DEVICE = 'cuda:0'
+    device = os.get_environ("DEVICE_OVERRIDE")
+    DEVICE = ["cpu" if device=="cpu" else "cuda:0"][0]
     MODEL_PATH = os.path.join(PSICHIC_PATH, 'trained_weights', 'PDBv2020_PSICHIC')
     BATCH_SIZE = 128
     
