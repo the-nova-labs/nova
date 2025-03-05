e4f8a795951fb48a768ecec4cc74fb06a7037656
cosmic-amanita <68027389+cosmic-amanita@users.noreply.github.com>
2025-03-05 10:49:37 -0500

    Update runtime_config.py

--- PSICHIC/runtime_config.py
+++ PSICHIC/runtime_config.py
@@ -3,7 +3,7 @@ import os
 
 class RuntimeConfig:
     PSICHIC_PATH = os.path.dirname(os.path.abspath(__file__))
-    device = os.get_environ("DEVICE_OVERRIDE")
+    device = os.environ.get("DEVICE_OVERRIDE")
     DEVICE = ["cpu" if device=="cpu" else "cuda:0"][0]
     MODEL_PATH = os.path.join(PSICHIC_PATH, 'trained_weights', 'PDBv2020_PSICHIC')
     BATCH_SIZE = 128
