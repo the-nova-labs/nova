c43c49db1abfc8f03b931b9505c2db24f86307ef
pycorn0729 <hayesdominique0729@gmail.com>
2025-03-16 10:22:34 -0500

    chore: style code

--- submit_results_mannual.py
+++ submit_results_mannual.py
@@ -1,6 +1,7 @@
 from my_utils import submit_results
 import json
 
+
 def submit_results_mannual(epoch_number: int):
     with open(f"results/submissions_epoch_{epoch_number}.json", "r") as f:
         commitments = json.load(f)        
@@ -8,6 +9,7 @@ def submit_results_mannual(epoch_number: int):
         submit_results(commitments)
     except Exception as e:
         print(e)
+
 
 if __name__ == "__main__":
     submit_results_mannual(14281)
